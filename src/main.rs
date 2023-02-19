88658813a50b00467193300e6f98295f54d814a4
Alvinosh <alvihysa89@gmail.com>
2023-02-19 17:12:06 +0100

    Polishing

--- src/main.rs
+++ src/main.rs
@@ -38,20 +38,42 @@ const SUFFIX: &'static str = r#" $
 
 fn main() {
     let args = Cli::parse();
+    println!("PARSING FILE...");
     let input_string = std::fs::read_to_string(args.input).unwrap();
     let lexer = Lexer::new(&input_string);
     let mut parser = parser::Parser::new(lexer.peekable());
     let tex = expression::evaulate(parser.parse().unwrap());
+    println!("FILE PARSED.");
 
-    let temp_file_name = args.output;
-    let mut temp = std::fs::File::create(format!("{}.tex", temp_file_name)).unwrap();
+    let temp_dir = std::env::temp_dir();
+    let output_path = std::path::Path::new(&args.output);
+    let file_name = output_path
+        .file_stem()
+        .expect("ERROR: Incorrect Output Path.");
 
-    temp.write(PREFIX.as_bytes()).unwrap();
-    temp.write(tex.as_bytes()).unwrap();
-    temp.write(SUFFIX.as_bytes()).unwrap();
+    let temp_file_path = temp_dir.join(file_name);
 
-    let term_command = format!("latex {0}.tex && dvipng -D 1000 {0}.dvi", temp_file_name);
+    let temp_file_paths = [
+        temp_file_path.with_extension("tex"),
+        temp_file_path.with_extension("dvi"),
+        temp_file_path.with_extension("log"),
+        temp_file_path.with_extension("aux"),
+    ];
 
+    let mut temp_tex = std::fs::File::create(&temp_file_paths[0]).unwrap();
+    temp_tex.write(PREFIX.as_bytes()).unwrap();
+    temp_tex.write(tex.as_bytes()).unwrap();
+    temp_tex.write(SUFFIX.as_bytes()).unwrap();
+
+    let term_command = format!(
+        "latex -output-directory={3} {0}  && dvipng -D 1000 -o {2} {1}",
+        &temp_file_paths[0].display(),
+        temp_file_paths[1].display(),
+        args.output,
+        temp_dir.display(),
+    );
+
+    println!("RUNNING: LATEX...");
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
             .args(["/C", &term_command])
@@ -64,6 +86,10 @@ fn main() {
             .output()
             .expect("failed to execute process")
     };
+    println!("RUNNING: COMPLETE.");
+    println!("GENERATED: {}", args.output);
 
-    println!("Generated {}1.png...", temp_file_name);
+    for file in temp_file_paths {
+        std::fs::remove_file(file).unwrap();
+    }
 }
